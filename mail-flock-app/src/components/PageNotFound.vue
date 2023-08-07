78ae0890f256f934bdc0bfaa8fed2e014012c1b4
Gagan Chordia <chordiagagan@gmail.com>
2023-08-07 12:35:57 +0530

    16. Added 404 Page

---
+++ mail-flock-app/src/components/PageNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="error-page">
+    <img
+      src="@/assets/error404.png"
+      alt="404 Page Not Found"
+      class="error-image"
+    />
+    <router-view />
+  </main>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "PageNotFound",
+};
+</script>
+
+<style scoped>
+.error-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.error-image {
+  max-width: 500px;
+}
+</style>
