a2fdad7f289ca23f0f7c98142d314f1195028406
Gagan Chordia <chordiagagan@gmail.com>
2023-08-25 19:56:27 +0530

    43b. Login Page

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -7,8 +7,9 @@
             <input type="text" v-model="username" id="username" required />
             <label for="password">Password</label>
             <input type="password" v-model="password" id="password" required />
-            <button type="submit">Login</button>
+            <button type="submit" class="prm-btn">Login</button>
         </form>
+        <router-link to="/register" class="link redirect">Don't have an account? Register</router-link>
     </section>
 </template>
 
@@ -52,7 +53,72 @@ export default {
     }
   },
   components: {
-    'vue-toast': VueToast
+    '': VueToast
   }
 }
 </script>
+
+<style scoped>
+.login-container,
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.login-container {
+  background-color: #f8f8f8;
+  align-items: center;
+  border: 2px solid #222;
+  border-radius: 5px;
+  padding: 20px;
+  width: max-content;
+  color: #222;
+  margin: 0 auto;
+}
+
+form {
+  align-items: start;
+  font-weight: 500;
+}
+
+label,
+input {
+  margin: 5px 0;
+}
+
+label {
+  font-weight: 500;
+}
+
+input {
+  padding: 10px 20px;
+  border-radius: 3px;
+  border: 2px solid #222;
+  width: 300px;
+  line-height: 20px;
+  background-color: #f8f8f8;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+  font-weight: 600;
+  color: #222;
+}
+
+input:focus{
+  border: 2px solid #4caf50;
+}
+
+button {
+  border: none;
+  width: 300px;
+  padding: 10px 20px;
+  margin: 10px auto;
+  font-weight: 600;
+  border-radius: 20px;
+}
+
+.redirect {
+  font-size: 16px;
+  margin: 5px 0;
+}
+</style>
